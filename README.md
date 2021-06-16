b1377cec0547ed767f432c80a21f99c132ed0c92
Arnab Mukherjee <arnabmukherjee@Arnabs-MacBook-Air.local>
2021-06-16 15:35:15 +0530

    engines update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SqlAlchemyTutorial
 Repo for Sql Alchemy tutorial 
+
+## Pointers
+* SqlAlchemy is a wrapper or ORM(Object relation Mapper) tool kit for python.
+
---
+++ engines.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, text
+
+
+engine = create_engine("sqlite:///:memory:")  # engine for info about data bases and connectors(dbapi)
+connection = engine.connect()  # creating connection, can be used with context manager.
+
+# creating table
+create_table = text("CREATE TABLE some_table (x int, y int)")
+connection.execute(create_table)
+
+# inserting data
+connection.execute(
+    text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+    [{"x": 1, "y": 3}, {"x": 2, "y": 4}]
+)
+
+# fetchall -> list of tuple, fetchone -> tuple
+result = connection.execute(text("select * from some_table"))
+# commit is not relevant in the context of in memory
+
+if __name__ == '__main__':
+    print(result.fetchone()[1])
