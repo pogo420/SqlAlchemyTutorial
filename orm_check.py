4f2b2a4893687843ede32c1b262d192b45d6ecd2
Arnab Mukherjee <arnabmukherjee@Arnabs-MacBook-Air.local>
2021-06-16 21:04:07 +0530

    orm check

--- orm_check.py
+++ orm_check.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine, and_, or_, select
+from sqlalchemy import Column, Integer, String, create_engine, and_, or_, select
 
 engine = create_engine("sqlite:///orm-test.db")
 
@@ -10,11 +10,24 @@ Session = sessionmaker(engine)
 class User(Base):
     __tablename__ = "user"
 
-    id = Column("id", Integer, primary_key=True)
+    id_ = Column("id", Integer, primary_key=True)
     name = Column("name", String)
+
+    def __init__(self, id_, name):
+        self.name = name
+        self.id_ = id_
 
 
 Base.metadata.create_all(engine)
-session = Session()
+session = Session()  # act as unit of work, all steps under a session are atomic in nature.
 
-arnab = User()
+for i in range(50):
+    session.add(User(i+1, f"Person{i}"))
+
+# ignoring for now
+# session.commit()
+# session.close()
+
+if __name__ == '__main__':
+    for user in session.query(User).filter(User.id_ > 30).filter(User.id_ < 40):
+        print(user.id_, user.name)
