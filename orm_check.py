385f5eae475f9b2d1e4e45e6109b8ebf3dd6d843
Arnab Mukherjee <arnabmukherjee@Arnabs-MacBook-Air.local>
2021-06-16 21:11:08 +0530

    orm check

--- orm_check.py
+++ orm_check.py
@@ -18,11 +18,12 @@ class User(Base):
         self.id_ = id_
 
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)  # creating metadata
 session = Session()  # act as unit of work, all steps under a session are atomic in nature.
 
-for i in range(50):
-    session.add(User(i+1, f"Person{i}"))
+# initial data/ for first run.
+# for i in range(50):
+#     session.add(User(i+1, f"Person{i}"))
 
 # ignoring for now
 # session.commit()
@@ -31,3 +32,6 @@ for i in range(50):
 if __name__ == '__main__':
     for user in session.query(User).filter(User.id_ > 30).filter(User.id_ < 40):
         print(user.id_, user.name)
+
+    session.commit()
+    session.close()
